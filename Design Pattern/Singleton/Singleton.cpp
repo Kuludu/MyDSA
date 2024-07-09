0139db8e6aa23646798d2090075889ed59564ef8
Yuxiang Fan <opensource@kuludu.net>
2024-07-09 11:14:09 +0800

    Update Singleton

--- Design Pattern/Singleton/Singleton.cpp
+++ Design Pattern/Singleton/Singleton.cpp
@@ -2,27 +2,35 @@
 #include <mutex>
 
 class Singleton {
+protected:
+    Singleton() = default; // default constructor
+    
+    Singleton(const Singleton&) = delete; // delete copy constructor
+    Singleton& operator=(const Singleton&) = delete; // delete copy assignment operator
+
+    Singleton(const Singleton&&) = delete; // delete move constructor
+    Singleton& operator=(const Singleton&&) = delete; // delete move assignment operator
+};
+
+class ConcreteSingleton final: public Singleton {
 private:
-    Singleton() {
+    ConcreteSingleton() {
         static std::mutex mtx;
         std::lock_guard<std::mutex> lock(mtx);
-        std::cout << "Singleton created." << std::endl;
+        std::cout << "ConcreteSingleton created." << std::endl;
     }
-    Singleton(const Singleton&) = delete; // delete copy constructor
-    const Singleton& operator=(const Singleton&) = delete; // delete copy assignment operator
-
 public:
-    static Singleton* getInstance() {
-        static Singleton instance;
+    static ConcreteSingleton* getInstance() {
+        static ConcreteSingleton instance;
         return &instance;
     }
 };
 
 int main() {
-    Singleton* s1 = Singleton::getInstance();
-    Singleton* s2 = Singleton::getInstance();
-    std::cout << "s1: " << s1 << std::endl;
-    std::cout << "s2: " << s2 << std::endl;
+    Singleton* concreteSingleton_1 = ConcreteSingleton::getInstance();
+    Singleton* concreteSingleton_2 = ConcreteSingleton::getInstance();
+    std::cout << "ConcreteSingleton1: " << concreteSingleton_1 << std::endl;
+    std::cout << "ConcreteSingleton2: " << concreteSingleton_2 << std::endl;
     
     return 0;
 }
