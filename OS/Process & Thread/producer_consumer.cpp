20881caf1fb97756adabaf3c3b7a38616b232dfd
Yuxiang Fan <opensource@kuludu.net>
2024-07-12 15:48:47 +0800

    Update Producer Consumer

--- OS/Process & Thread/producer_consumer.cpp
+++ OS/Process & Thread/producer_consumer.cpp
@@ -10,19 +10,21 @@ int main() {
     std::queue<int> buffer;
     const int MAX_BUFFER_SIZE = 5;
     
-    auto producer = [&](int id) {
-        for (int i = 0; ; ++i) {
+    auto producer = [&](std::stop_token st, int id) {
+        int i = 0;
+        while (!st.stop_requested()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulate production time
             std::unique_lock<std::mutex> lock(mtx); // Acquire lock
             cv.wait(lock, [&]() { return buffer.size() < MAX_BUFFER_SIZE; }); // Wait for buffer to be empty
-            std::cout << "Producer " << id << " produced: " << i << std::endl; 
+            std::cout << "Producer " << id << " produced: " << i++ << std::endl; 
             buffer.push(i); // Push to buffer
             cv.notify_all(); // Notify consumers or other producers
         }
+        std::cout << "Producer " << id << " stopped" << std::endl; 
     };
 
-    auto consumer = [&](int id) {
-        for (;;) {
+    auto consumer = [&](std::stop_token st, int id) {
+        while (!st.stop_requested()) {
             std::unique_lock<std::mutex> lock(mtx); // Acquire lock
             cv.wait(lock, [&]() { return !buffer.empty(); }); // Wait for buffer to be non-empty
             int item = buffer.front();
@@ -32,21 +34,32 @@ int main() {
             cv.notify_all(); // Notify producers or other consumers
             std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Simulate consumption time
         }
+        std::cout << "Consumer " << id << " stopped" << std::endl; 
     };
 
-    std::thread producer_thread[2];
-    std::thread consumer_threads[4];
+    std::jthread producer_thread[2];
+    std::jthread consumer_threads[4];
     for (int i = 0; i < 2; ++i) {
-        producer_thread[i] = std::thread(producer, i);
+        producer_thread[i] = std::jthread(producer, i);
     }
     for (int i = 0; i < 4; ++i) {
-        consumer_threads[i] = std::thread(consumer, i);
+        consumer_threads[i] = std::jthread(consumer, i);
     }
-    for (int i = 0; i < 2; ++i) {
-        producer_thread[i].join();
-    }
-    for (int i = 0; i < 4; ++i) {
-        consumer_threads[i].join();
+
+    std::string operation; 
+    while (std::cin >> operation && operation != "exit") {
+        if (operation =="quit_producer") {
+            for (int i = 0; i < 2; ++i) {
+                producer_thread[i].request_stop();
+            }
+            std::cout << "Producer threads stopped" << std::endl;
+        }
+        if (operation == "quit_consumer") {
+            for (int  i = 0; i < 4; i++) {
+                consumer_threads[i].request_stop();
+            }
+            std::cout << "Consumer threads stopped" << std::endl;
+        }
     }
 
     return 0;
