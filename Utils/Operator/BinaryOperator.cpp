90b974098a13393fb0d322f42e797b6967ea0c20
Yuxiang Fan <opensource@kuludu.net>
2025-01-19 16:04:14 +0800

    Update Divide

--- Utils/Operator/BinaryOperator.cpp
+++ Utils/Operator/BinaryOperator.cpp
@@ -4,7 +4,7 @@ template<typename U, typename V>
 class BinaryOperator {
 public:
     BinaryOperator(U a, V b) : element_a(a), element_b(b) {};
-    virtual int execute() = 0;
+    virtual V execute() = 0;
 protected:
     U element_a;
     V element_b;
--- Utils/Operator/Divide.cpp
+++ Utils/Operator/Divide.cpp
@@ -1,26 +1,29 @@
 #include "BinaryOperator.cpp"
 #include <type_traits>
+#include <stdexcept>
 
-template<typename U, typename V>
+template<typename U, typename V,
+         typename = typename std::enable_if<
+            (std::is_integral_v<U> || std::is_floating_point_v<U>) &&
+            (std::is_integral_v<V> || std::is_floating_point_v<V>)>::type>
 class Divide : public BinaryOperator<U, V> {
 private:
     using BinaryOperator<U, V>::element_a;
     using BinaryOperator<U, V>::element_b;
 public:
-    Divide(U a, V b) : BinaryOperator<U, V>(a, b) {};
-    auto execute() -> decltype(element_a / element_b) {
-        if (std::is_integral_v<V>) {
-            if (element_b == 0) {
+    Divide(U a, V b) : BinaryOperator<U, V>(a, b) {
+        if constexpr (std::is_integral_v<V>) {
+            if (0 == element_b) {
                 throw std::runtime_error("Cannot divide by zero.");
             }
-        } else if (std::is_floating_point_v<V>) {
-            if (element_b == 0.0) {
+        } else if constexpr (std::is_floating_point_v<V>) {
+            if (0.0 == element_b) {
                 throw std::runtime_error("Cannot divide by zero.");
             }
-        } else {
-            throw std::runtime_error("Invalid type for division.");
         }
+    }
 
+    V execute() override {
         return element_a / element_b;
     }
 };
